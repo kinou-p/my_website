e65bc0754cef7b72b565fdbcbf00efcb88104cb2
kinou-p <alexpomms@gmail.com>
2024-05-03 09:34:32 +0200

    started home need to do trad

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,49 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/first.jpg';
+import './css/App.css';
+import './css/mine.css';
+
+import MenuSimple from './components/language';
+
 
 function App() {
+
+  // function createHandleMenuClick(arg0: string): any {
+  //   // throw new Error('Function not implemented.');
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <header>
+            <div className="header-content">
+                <div className="logo">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <h1 className="title">A.P</h1>
+                <MenuSimple/>
+            </div>
+        </header>
+        <div className='home'>
+          <div className="presentation">
+              <div className='hello'>Welcome.</div>
+              <p>Étudiant a 42 en informatique, je cherche un emploi afin de compléter 
+                ma formation académique par une expérience professionnelle qui a du sens. 
+                Je suis ouvert à de nombreuses propositions en rapport avec mes études 
+                et suis prêt à discuter de mon parcours plus amplement lors d’un entretien.</p>
+          </div>
+          <div className="project">
+              <div className='project_txt'>Projects</div>
+              <p>- capjwdalw</p>
+              <p>- capjwdalw</p>
+              <p>- capjwdalw</p>
+              <p>- capjwdalw</p>
+              <p>- capjwdalw</p>
+
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
